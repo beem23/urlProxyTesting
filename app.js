f5be284dcba08d08bd95896206dec35de9472468
Marlon Bellot <marlonbellot4@gmail.com>
2023-01-11 16:21:13 -0500

    rework proxy

--- app.js
+++ app.js
@@ -7,30 +7,65 @@ app.use(cors());
 app.use(express.json());
 
 app.all('/proxy', (req, res) => {
+    console.log('connected')
     // get the target URL from the query parameter
     const targetUrl = req.query.url;
 
     // forward the incoming request to the target server
-    const proxy = request({
-        url: targetUrl,
-        method: req.method,
-        json: req.body,
-        headers: req.headers
+    request(targetUrl, { method: req.method, json: req.body, headers: req.headers }, (error, response, body) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Error occurred while forwarding the request');
+        } else {
+            // Add the necessary CORS headers to the response
+            res.set('Access-Control-Allow-Origin', '*');
+            res.set('Access-Control-Allow-Methods', 'GET, POST');
+            res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+            res.status(response.statusCode).json(body);
+        }
     });
-
-    // Add the necessary CORS headers to the response
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Methods', 'GET, POST');
-    res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-    // pipe the response back to the client
-    proxy.pipe(res);
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Proxy server listening on port ${port}`);
 });
+
+// const express = require('express');
+// const request = require('request');
+// const cors = require('cors');
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.all('/proxy', (req, res) => {
+//     console.log('connected')
+//     // get the target URL from the query parameter
+//     const targetUrl = req.query.url;
+
+//     // forward the incoming request to the target server
+//     const proxy = request({
+//         url: targetUrl,
+//         method: req.method,
+//         json: req.body,
+//         headers: req.headers
+//     });
+
+//     // Add the necessary CORS headers to the response
+//     res.set('Access-Control-Allow-Origin', '*');
+//     res.set('Access-Control-Allow-Methods', 'GET, POST');
+//     res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+//     // pipe the response back to the client
+//     proxy.pipe(res);
+// });
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//     console.log(`Proxy server listening on port ${port}`);
+// });
 
 // const express = require('express');
 // const request = require('request');
