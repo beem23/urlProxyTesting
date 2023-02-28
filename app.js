cfc6b1219ffa27f276663d7a1214be80d0a66258
Marlon Bellot <marlonbellot4@gmail.com>
2023-02-28 02:27:50 -0500

    rebooting

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const rateLimit = require("express-rate-limit");
 app.use(cors());
 app.use(express.json());
 
+
 app.use(rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
